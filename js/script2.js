5aed21695bc5880b7dceb5a695b71aa3f29998cf
Katarinaphox <Katarinaphox@mail.ru>
2018-05-15 17:33:32 +0300

    functions add, sub, div, mul

---
+++ js/script2.js
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+function add(a, b) {
+	if (typeof a != 'number') {
+		alert("type of a = " + typeof a);
+
+	} else if (typeof b != 'number') {
+		alert("type of b = " + typeof b)
+
+	} else {
+		document.write(a + b);
+	}
+}
+
+function sub(x, y) {
+	if (typeof x != 'number') {
+		alert("type of x = " + typeof x);
+
+	} else if (typeof y != 'number') {
+		alert("type of y = " + typeof y)
+
+	} else {
+		console.log(x - y);
+	}
+}
+
+function div(n, k) {
+	if (typeof n != 'number') {
+		alert("type of n = " + typeof n);
+
+	} else if (typeof k != 'number' && k <= 0) {
+		alert("incorrect input")
+
+	} else {
+		console.log(n/k));
+	}
+}
+
+function div(с, v) {
+	if (typeof c != 'number') {
+		alert("type of c = " + typeof c);
+
+	} else if (typeof v != 'number') {
+		alert("type of v = " + typeof v)
+
+	} else {
+		console.log(c*v);
+	}
+}
+
